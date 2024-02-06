7eb15ffe90cb3f6d5f833c8f10d34c86233b5f63
love3nt <love3ntt@gmail.com>
2024-02-06 13:32:11 -0800

    quick code on formatting outputs

---
+++ Mod 3 Formatting Outputs.py
@@ -0,0 +1,14 @@
+employee1 = 'Ana Borjas'
+wages1 = 345.60
+employee2 = 'Christopher Silverton'
+wages2 = 5350.99
+
+
+# instead of this :
+print(employee1,wages1)
+print(employee2,wages2)
+print()
+
+# we can format it like this, for clean asthetic
+print(format(employee1,'23s'),format(wages1,'8.2f'))
+print(format(employee2,'23s'),format(wages2,'8.2f'))
