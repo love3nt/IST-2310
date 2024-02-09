a4e66530fe30e0429364c6b2550a96a13071f1b3
love3nt <love3ntt@gmail.com>
2024-02-08 16:16:11 -0800

    Declaring multiple variables at once

--- testing multi variables.py
+++ testing multi variables.py
@@ -1,5 +1,5 @@
 
 
-first_name, last_name = input('Please enter your first name: '), input('Please enter your last name: ')
+first_name, middle_name, last_name = input('Please enter your first name: '), input('Please enter your middle name: '), input('Please enter your last name: ')
 
-print(f'Your name is {first_name} {last_name}')
+print(f'Your name is {first_name} {middle_name} {last_name}')
