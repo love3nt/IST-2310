c60dc1aac34732bda86f157f8c863e9591a4f698
love3nt <love3ntt@gmail.com>
2024-02-15 17:17:14 -0800

    Updated to Ver. 1.0

--- Project 1.py
+++ Project 1.py
@@ -24,44 +24,96 @@ import os
 
 
 
-# Creating a clear screen function using imported modules and defining the function.
+
+#############################################
+###         TIMED WELCOME MESSAGE         ###
+#############################################
+
 
 def greeting_message(message, n):
     print(message)
     time.sleep(n)
-    # Clearing the screen
     os.system('cls' if os.name == 'nt' else 'clear')
 
-# This message will display for 5 seconds, clear screen, then proceed to main program loop
-    
-message = "\nWelcome!\n \nThis program calculates appropriate discount based on quantity purchased and corresponding sales tax based on state"
+message = '\nWelcome!\n \nThis program calculates appropriate discount based on quantity purchased and corresponding sales tax based on state'
 n = 5          
-greeting_message(message,n)
+greeting_message(message, n)
 
-# Begin Main Program Loop
+
+#############################################
+###         DECLARING FUNCTIONS           ###
+#############################################
+
+
+# This will calculate the state tax based on what state was entered.
+def calculate_state_tax(valid_state):
+    if valid_state == 'CA':
+        return 0.20
+    elif valid_state == 'AZ':
+        return 0.10
+    elif valid_state == 'NV':
+        return 0.05
+    else:
+        return None
+
+# This will calculate the discount to be applied based on the quantity of items entered.
+def calculate_discount(item_quantity):
+    if item_quantity >= 20:
+        return 0.10
+    elif item_quantity >= 10:
+        return 0.05
+    else:
+        return 0
+
+
+#############################################
+###           MAIN PROGRAM LOOP           ###
+#############################################
 
 run = True
-while run == True:
+while run:
 
-    # Prompt the user to input the number of items purchased
+    # Prompt the user to input the quantity of items & state, if anything other than the valid states acceptable or a number less than 0, then perform error check.
+    item_quantity = int(input('Enter the quantity of items purchased: '))
 
-    item_quanitity = int(input('Enter the quanity of items purchased: '))
+    # Loop error check, until acceptable input is inputted.
+    while item_quantity <= 0:
+        item_quantity = int(input('Invalid Input! Quantity must be greater than 0: '))
 
-    # Error check for quanity less then or equal to 0
-    while item_quanitity <= 0:
-        item_quanitity = int(input('Invalid Input! Quanity must be greater than 0: '))
+    valid_state = input('Enter a valid state (CA, NV, AZ): ').upper()
 
-     # Prompt the user to input a valid state (CA, AZ. NV)
-        
-    valid_state = str(input('Enter a valid state (CA, NV, AZ): ')).upper
+    while valid_state not in ('CA', 'NV', 'AZ'):
+        valid_state = input('Invalid State! Enter a valid state (CA, NV, AZ): ').upper()
 
-    # Error check for a valid state 
-    while valid_state not in (CA, NV, AZ):
-        valid_state = str(input('Invalid State! Enter a valid state (CA, NV, AZ): '))
+    state_tax = calculate_state_tax(valid_state)
+    items_discount = calculate_discount(item_quantity)
 
-    if valid_state == CA:
-        
+    gross_cost = item_quantity * 1
+    discount_amount = gross_cost * items_discount
+    net_cost = gross_cost - discount_amount
+    tax_amount = net_cost * state_tax
+    after_tax = net_cost + tax_amount
 
-    
-        
 
+    # Discount applied identifier
+
+    if item_quantity > 9 and item_quantity < 20:
+	    print('\n5% discount\n')
+    elif item_quantity > 19:
+	    print('\n10% discount\n')
+    else:
+         print('No Discount Applied')
+
+
+    # Format & Print the calculations - This will format each output respective to their own value.
+    print('{:<16s} {:>8.2f}'.format('Gross Cost:', gross_cost))
+    print('{:<16s} {:>8.2f}'.format('Discount:', discount_amount))
+    print('{:<16s} {:>8.2f}'.format('Net Cost:', net_cost))
+    print('{:<16s} {:>8.2f}'.format('Tax:', tax_amount))
+    print('{:<16s} {:>8.2f}'.format('After Tax:', after_tax))
+
+
+
+    # End the main program loop 
+
+    run = False
